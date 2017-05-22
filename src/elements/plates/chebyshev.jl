efca2146d77d78ef75481dacbf2d2c4bba1def0f
Darwin Darakananda <darwindarak@gmail.com>
2017-05-22 10:07:03 -0700

    feature: add inverse discrete Chebyshev transforms

--- src/elements/plates/chebyshev.jl
+++ src/elements/plates/chebyshev.jl
@@ -1,4 +1,34 @@
 """
+    chebyshev_nodes(N, T = Float64)
+
+Gives `N` extrema Chebyshev points of type `T` in [-1 ,1].
+"""
+function chebyshev_nodes(N, T = Float64)
+    T[cos(θ) for θ in linspace(π, 0, N)]
+end
+
+"""
+    eval_cheb(C, s)
+
+Evaluate a Chebyshev series with coefficients `C` at point(s) `s`.
+
+`s` can be either a single number or an array of numbers.
+"""
+eval_cheb(C, ss) = [eval_cheb(C, s) for s in ss]
+function eval_cheb(C::AbstractArray{U}, s::S)::U where {U,S <: Real}
+    -1 ≤ s ≤ 1 || throw(DomainError("s ∉ [-1,1]"))
+    T₋ = s
+    T  = one(S)
+
+    f = zero(U)
+    for c in C
+        f += c*T
+        T₋, T = T, 2s*T - T₋
+    end
+    f
+end
+
+"""
     chebyshev_transform!{T}(C::T, x::T, plan! = FFTW.plan_r2r!(C, FFTW.REDFT00))
 
 Compute the discrete Chebyshev transform of `x` and store the result in `C` (s = -1 → x[1], s = 1 → x[end]).
@@ -32,6 +62,45 @@ function chebyshev_transform(x)
 end
 
 chebyshev_transform!(x, plan! = FFTW.plan_r2r!(x, FFTW.REDFT00)) = chebyshev_transform!(x, x, plan!)
+
+"""
+    inv_chebyshev_transform!{T}(x::T, C::T, plan! = FFTW.plan_r2r!(C, FFTW.REDFT00))
+
+Evaluate the Chebyshev series with coefficients `C` on the [extrema
+nodes](@ref chebyshev_nodes) and store the results in `x`.
+
+It is the inverse of [`chebyshev_transform!`](@ref), and similarly
+takes an optional `plan!` argument for a preplanned discrete cosine
+transform.
+"""
+function inv_chebyshev_transform!{T}(x::T, C::T, plan! = FFTW.plan_r2r!(x, FFTW.REDFT00))
+    N = length(C)
+    x[1] = C[1]
+    for n in 2:2:N-2
+        x[n]   = -0.5C[n]
+        x[n+1] =  0.5C[n+1]
+    end
+    if iseven(N)
+        x[N] = -C[N]
+    else
+        x[N-1] = -0.5C[N-1]
+        x[N] = C[N]
+    end
+
+    plan!*x
+end
+inv_chebyshev_transform!(C, plan! = FFTW.plan_r2r!(C, FFTW.REDFT00)) = inv_chebyshev_transform!(C, C, plan!)
+
+"""
+    inv_chebyshev_transform(C)
+
+Inverts the coefficients `C` from a [discrete Chebyshev transform](@ref chebyshev_transform).
+"""
+function inv_chebyshev_transform(C)
+    x = zeros(eltype(C), length(C))
+    inv_chebyshev_transform!(x, C)
+    return x
+end
 
 """
     clenshaw_curtis_weights(N)
