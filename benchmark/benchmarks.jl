bcb807954d6e16be6b4f7316c0642edd016fccad
Darwin Darakananda <darwindarak@gmail.com>
2017-10-11 20:27:05 -0700

    Update benchmark functions [ci skip]

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,29 +1,31 @@
-using VortexModel
+using PotentialFlow
 
 @benchgroup "Pairwise-dispatch" begin
     N = 500
+    t = 0.0
+
     points = Vortex.Point.(rand(Complex128, N), rand(N))
     sheet  = Vortex.Sheet(rand(Complex128, N), accumulate(+, rand(N)), rand())
     blobs = sheet.blobs
 
     z = rand(Complex128)
 
-    @bench "points to position" Vortex.induce_velocity(z, points)
-    @bench "group to position" Vortex.induce_velocity(z, (points, blobs, sheet))
-    @bench "nested group to position" Vortex.induce_velocity(z, (points, blobs, sheet))
+    @bench "points to position" induce_velocity(z, points, t)
+    @bench "group to position" induce_velocity(z, (points, blobs, sheet), t)
+    @bench "nested group to position" induce_velocity(z, (points, blobs, sheet), t)
 
     zs = getfield.(points, :z)
-    @bench "points to positions" Vortex.induce_velocity(zs, points)
-    @bench "sheet to positions" Vortex.induce_velocity(zs, sheet)
+    @bench "points to positions" induce_velocity(zs, points, t)
+    @bench "sheet to positions" induce_velocity(zs, sheet, t)
 
-    wss = Vortex.allocate_velocity(points)
-    @bench "in-place sheet to points" Vortex.induce_velocity!(wss, points, sheet)
+    wss = allocate_velocity(points)
+    @bench "in-place sheet to points" induce_velocity!(wss, points, sheet, t)
 
-    @bench "in-place group to points" Vortex.induce_velocity!(wss, points, (blobs, sheet))
+    @bench "in-place group to points" induce_velocity!(wss, points, (blobs, sheet), t)
 
     sys = (sheet, blobs)
-    ws = Vortex.allocate_velocity(sys)
-    @bench "in-place points to group" Vortex.induce_velocity!(ws, sys, points)
+    ws = allocate_velocity(sys)
+    @bench "in-place points to group" induce_velocity!(ws, sys, points, t)
 
-    @bench "self-induced velocities" Vortex.self_induce_velocity!(ws, sys)
+    @bench "self-induced velocities" self_induce_velocity!(ws, sys, t)
 end
