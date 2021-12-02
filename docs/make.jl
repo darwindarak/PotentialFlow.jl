bc8fd6a004564567179c44d07f282f96e5adb8b6
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-12-02 14:04:49 -0800

    Updated docs/make.jl a bit

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,11 @@
 using Documenter, PotentialFlow
 
+ENV["GKSwstype"] = "nul" # removes GKS warnings during plotting
+
 makedocs(
     sitename = "PotentialFlow.jl",
+    doctest = true,
+    clean = true,
     pages = [
         "Home" => "index.md",
         "Manual" => ["manual/quickstart.md",
@@ -13,12 +17,28 @@ makedocs(
                      ],
         "Internals" => [ "internals/properties.md"]
     ],
-    format = Documenter.HTML(assets = ["assets/custom.css"]),
+    #format = Documenter.HTML(assets = ["assets/custom.css"]),
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        mathengine = MathJax(Dict(
+            :TeX => Dict(
+                :equationNumbers => Dict(:autoNumber => "AMS"),
+                :Macros => Dict()
+            )
+        ))
+    ),
 #   strict = true
 )
 
-if "DOCUMENTER_KEY" in keys(ENV)
-    deploydocs(;
-     repo = "github.com/darwindarak/PotentialFlow.jl.git",
+#if "DOCUMENTER_KEY" in keys(ENV)
+    #deploydocs(;
+    # repo = "github.com/darwindarak/PotentialFlow.jl.git",
+    #)
+deploydocs(
+    repo = "github.com/darwindarak/PotentialFlow.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing
+    #versions = "v^"
     )
-end
+#end
