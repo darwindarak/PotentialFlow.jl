d58eca5875deba8c3f9ff9c03859d5ce8ae1dbb0
Jeff Eldredge <jdeldre@g.ucla.edu>
2022-08-25 12:04:37 -0700

    Updated the conformal mapping notebook to include LinkedLines example

--- src/elements/Bodies.jl
+++ src/elements/Bodies.jl
@@ -17,7 +17,7 @@ import ..Elements: position, impulse, circulation
 import ..Motions: induce_velocity, induce_velocity!, mutually_induce_velocity!, self_induce_velocity,
                   self_induce_velocity!, allocate_velocity, advect!, advect, streamfunction, complexpotential
 import SchwarzChristoffel: Polygon, ExteriorMap, ConformalMap, PowerMap, addedmass,
-                            InverseMap, DerivativeMap, coefficients
+                            InverseMap, DerivativeMap, coefficients, LinkedLines
 
 import ..Utils:@get, MappedVector
 
